a6bd296d5cd32c076ac66a366ce4e465b4ca367c
Artem <artm.koshurnikov00@mail.ru>
2026-02-28 08:02:39 +0300

    JS syntax

--- src/App.js
+++ src/App.js
@@ -4,23 +4,52 @@ import { createElement } from 'react';
 
 export const App = () => {
 	const yearDate = new Date();
-	return (
-		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<span>Текущий год: {yearDate.getFullYear()}</span>
-			</header>
-		</div>
-	); // вся функция является декларативным стилем
+	// return (
+	// 	<div className="App">
+	// 		<header className="App-header">
+	// 			<img src={logo} className="App-logo" alt="logo" />
+	// 			<p>
+	// 				Edit <code>src/App.js</code> and save to reload.
+	// 			</p>
+	// 			<a
+	// 				className="App-link"
+	// 				href="https://reactjs.org"
+	// 				target="_blank"
+	// 				rel="noopener noreferrer"
+	// 			>
+	// 				Learn React
+	// 			</a>
+	// 			<span>Текущий год: {yearDate.getFullYear()}</span>
+	// 		</header>
+	// 	</div>
+	// ); // вся функция является декларативным стилем
+	const app = createElement(
+		'div',
+		{ className: 'app' },
+		createElement(
+			'header',
+			{ className: 'App-header' },
+			createElement('img', { src: logo, className: 'App-logo', alt: 'logo' }),
+			createElement(
+				'p',
+				null,
+				'Edit ',
+				createElement('code', null, 'src/App.js<'),
+				'and save to reload.',
+			),
+			createElement(
+				'a',
+				{
+					className: 'App-link',
+					href: 'https://reactjs.org',
+					target: '_blank',
+					rel: 'noopener noreferrer',
+				},
+				'Learn React',
+			),
+			createElement('span', null, `Текущий год: ${yearDate.getFullYear()}`),
+		),
+	);
+
+	return app;
 };
